b9b5976b1a73f8d128d05c09c47fae214ff03b1c
Phil Black <philosophicalblackhole@gmail.com>
2026-01-04 03:55:27 -0600

    Add PricingPage.jsx as a wrapper for PricingPage.tsx
    This file re-exports the PricingPage component from PricingPage.tsx for convenience.

---
+++ src/pages/PricingPage.jsx
@@ -0,0 +1,8 @@
+/**
+ * Wrapper: PricingPage (JSX)
+ * Purpose: Provide a JSX entry point as requested by Stripe docs.
+ * Note: Our primary implementation is in PricingPage.tsx; this file re-exports it for convenience.
+ */
+
+import PricingPage from './PricingPage'
+export default PricingPage
